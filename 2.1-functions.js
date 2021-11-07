6549986ccff81a61f24b9497c7f8696bbeacd1f3
sarit-chaet-hudis <sarit.chh@gmail.com>
2021-11-07 12:53:00 +0200

    2.1

---
+++ 2.1-functions.js
@@ -0,0 +1,7 @@
+function city (name, pop, capital) {
+    return name + " has " + pop + " million ppl and the capital is " + capital
+}
+
+let cityDesc = city("Israel", 8, "Jerus");
+
+console.log(cityDesc);
