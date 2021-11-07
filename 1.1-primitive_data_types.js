24ae03ec65f68e2b26714d7964aaa57d38af6763
sarit-chaet-hudis <sarit.chh@gmail.com>
2021-11-07 11:17:37 +0200

    primitive data types

---
+++ 1.1-primitive_data_types.js
@@ -0,0 +1,18 @@
+let s = "string";
+let n = 6;
+let obj = {
+    name:'my object',
+    age:18
+};
+let b = true;
+let nulli = null;
+let u;
+let sym = Symbol();
+
+console.log(typeof(s));
+console.log(typeof(n));
+console.log(typeof(obj));
+console.log(typeof(b));
+console.log(typeof(nulli));
+console.log(typeof(u));
+console.log(typeof(sym));
