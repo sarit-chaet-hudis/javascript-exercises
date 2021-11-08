b6895a3cb0793389811cdf066469db3a39bd7f90
sarit-chaet-hudis <sarit.chh@gmail.com>
2021-11-08 13:19:56 +0200

    siblings

---
+++ 4.1-siblings.js
@@ -0,0 +1,12 @@
+let s = window.prompt("how many siblings do you have?");
+
+
+if (s === 1) {
+    console.log('1 sibling!');
+}
+
+if (s > 1) {
+    console.log('More than 1 sibling!');
+} else {
+    console.log('No siblings');
+}
