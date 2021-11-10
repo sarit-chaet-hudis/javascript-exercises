72492725138d6842813f9be7e83d0f78d74eb06a
sarit-chaet-hudis <sarit.chh@gmail.com>
2021-11-10 15:10:24 +0200

    count letter frequencies returned in object

---
+++ 8.4-count_the_letters.js
@@ -0,0 +1,23 @@
+const array = ["Hello", "Good Day", "Your Welcome", "hotdog", "hamburgers"];
+
+const addProperty = (obj, k, v) => {
+  return {
+    ...obj,
+    [k]: v,
+  };
+};
+
+function countTheLetters(arr) {
+  const strArray = array.join("").replace(/\s+/g, "");
+  const resultObject = {};
+
+  for (var i = 0; i < strArray.length; i++) {
+    let charKey = strArray[i].toLowerCase();
+    [charKey] in resultObject ?
+    resultObject[charKey] += 1 :
+    resultObject[charKey] = 1 ;
+  }
+  console.log(resultObject);
+}
+
+countTheLetters(array);
