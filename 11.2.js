a00fd0318375b217646e7c8ca7b8450d7b88eaf6
sarit-chaet-hudis <sarit.chh@gmail.com>
2021-11-15 11:54:57 +0200

    11.2 part1

---
+++ 11.2.js
@@ -0,0 +1,46 @@
+function doubleValues(arr) {
+  return arr.map(function (n) {
+    return n * 2;
+  });
+}
+
+console.log(doubleValues([1, 5, 20]));
+
+function onlyEvenValues(arr) {
+  //returns a new array with only the even values in
+  // the array passed to the function
+  const evenValues = [];
+  arr.forEach((element) => {
+    if (element % 2 === 0) {
+      evenValues.push(element);
+    }
+  });
+  return evenValues;
+}
+
+console.log(onlyEvenValues([1, 5, 20]));
+
+function showFirstAndLast(arr) {
+  const firstAndLast = [];
+  for (let i = 0; i < arr.length; i++) {
+    //look for first string
+    if (typeof arr[i] === "string") {
+      firstAndLast.push(arr[i]);
+      break;
+      //only needs 1
+    }
+  }
+
+  for (let i = arr.length; i > 0; i--) {
+    //look for last string
+    if (typeof arr[i] === "string") {
+      firstAndLast.push(arr[i]);
+      break;
+    }
+    //only needs 1
+  }
+  return firstAndLast;
+}
+
+console.log(showFirstAndLast([1,2,"hi","there",3,"stranger",10.5]));
+
