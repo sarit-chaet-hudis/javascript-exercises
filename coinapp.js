450fda9870a6b8a238be58b2dd4e345d86c56f46
sarit-chaet-hudis <sarit.chh@gmail.com>
2021-11-30 11:51:41 +0200

    coin demo codealon

---
+++ coinapp.js
@@ -0,0 +1,34 @@
+function isTouching(a, b) {
+  const aRect = a.getBoundingClientRect();
+  const bRect = b.getBoundingClientRect();
+
+  return !(
+    aRect.top + aRect.height < bRect.top ||
+    aRect.top > bRect.top + bRect.height ||
+    aRect.left + aRect.width < bRect.left ||
+    aRect.left > bRect.left + bRect.width
+  );
+}
+
+const avatar = document.querySelector("#player");
+
+window.addEventListener("keyup", function (e) {
+  if (e.key === "ArrowDown" || e.key === "Down") {
+    const currTop = extractPos(avatar.style.top);
+    avatar.style.top = `${currTop + 50}px`;
+  } else if (e.key === "ArrowUp" || e.key === "Up") {
+    const currTop = extractPos(avatar.style.top);
+    avatar.style.top = `${currTop - 50}px`;
+  } else if (e.key === "ArrowRight" || e.key === "Right") {
+    const currLeft = extractPos(avatar.style.left);
+    avatar.style.left = `${currLeft + 50}px`;
+  } else if (e.key === "ArrowLeft" || e.key === "Left") {
+    const currLeft = extractPos(avatar.style.left);
+    avatar.style.left = `${currLeft - 50}px`;
+  }
+});
+
+const extractPos = (pos) => {
+  if (!pos) return 0;
+  return parseInt(pos.slice(0, -2));
+};
