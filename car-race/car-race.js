d844ff269189cfa7fa7e50432c3d5981d229548e
sarit-chaet-hudis <sarit.chh@gmail.com>
2021-11-30 22:05:03 +0200

    refactor to finish when car reaches finish

--- car-race/car-race.js
+++ car-race/car-race.js
@@ -4,27 +4,25 @@ restartButton.style.display = "none";
 
 function go(e) {
   if (e.key.toLowerCase() === "s") {
-    const car = document.querySelector("#player1-race .active");
-    if (car.classList.contains("finish")) {
+    const redCar = document.querySelector("#player1-race .active");
+    if (redCar.nextElementSibling.classList.contains("finish")) {
       document.querySelector("h2").innerText = "Red Player Won!!";
       window.removeEventListener("keyup", go);
       restartButton.style.display = "inline-block";
       restartButton.addEventListener("click", () => window.location.reload());
-      return;
     }
-    car.classList.remove("active");
-    car.nextElementSibling.classList.add("active");
+    redCar.classList.remove("active");
+    redCar.nextElementSibling.classList.add("active");
   }
   if (e.key.toLowerCase() === "l") {
-    const car = document.querySelector("#player2-race .active");
-    if (car.classList.contains("finish")) {
+    const yellowCar = document.querySelector("#player2-race .active");
+    if (yellowCar.nextElementSibling.classList.contains("finish")) {
       document.querySelector("h2").innerText = "Yellow Player Won!!";
       window.removeEventListener("keyup", go);
       restartButton.style.display = "inline-block";
       restartButton.addEventListener("click", () => window.location.reload());
-      return;
     }
-    car.classList.remove("active");
-    car.nextElementSibling.classList.add("active");
+    yellowCar.classList.remove("active");
+    yellowCar.nextElementSibling.classList.add("active");
   }
 }
