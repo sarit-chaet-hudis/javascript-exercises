980887b445c4368c0c2ac2cc26a36517368af506
sarit-chaet-hudis <sarit.chh@gmail.com>
2021-12-01 09:53:29 +0200

    refactor colors and clean code with finctions

--- car-race/car-race.js
+++ car-race/car-race.js
@@ -6,10 +6,7 @@ function go(e) {
   if (e.key.toLowerCase() === "s") {
     const redCar = document.querySelector("#player1-race .active");
     if (redCar.nextElementSibling.classList.contains("finish")) {
-      document.querySelector("h2").innerText = "Red Player Won!!";
-      window.removeEventListener("keyup", go);
-      restartButton.style.display = "inline-block";
-      restartButton.addEventListener("click", () => window.location.reload());
+      win("Red");
     }
     redCar.classList.remove("active");
     redCar.nextElementSibling.classList.add("active");
@@ -17,12 +14,18 @@ function go(e) {
   if (e.key.toLowerCase() === "l") {
     const yellowCar = document.querySelector("#player2-race .active");
     if (yellowCar.nextElementSibling.classList.contains("finish")) {
-      document.querySelector("h2").innerText = "Yellow Player Won!!";
-      window.removeEventListener("keyup", go);
-      restartButton.style.display = "inline-block";
-      restartButton.addEventListener("click", () => window.location.reload());
+      win("Yellow");
     }
     yellowCar.classList.remove("active");
     yellowCar.nextElementSibling.classList.add("active");
   }
+}
+
+function win(winner) {
+  const whoWon = document.querySelector(".whoWon");
+  whoWon.innerText = winner + " Player Won!!";
+  whoWon.style.color = winner === "Red" ? "#FC1014" : "#FFC55A";
+  window.removeEventListener("keyup", go);
+  restartButton.style.display = "inline-block";
+  restartButton.addEventListener("click", () => window.location.reload());
 }
